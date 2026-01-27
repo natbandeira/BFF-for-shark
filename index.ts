75aae6aee894365f5511e361233e1f282eb0818a
aneycan <nbandeira7@gmail.com>
2026-01-27 14:11:23 -0300

    inicia método que cria novo processo

--- index.ts
+++ index.ts
@@ -1,20 +1,52 @@
-const apiUrl: string = 'https://api-shark.serb-mg.com.br';
+class IniciaNovaClassificação {
 
-async function login(email: string, password: string): Promise<string> {
-    const response = await fetch(`${apiUrl}/login`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email, password }),
-    });
+    async login(): Promise<string> {
+        const response = await fetch(`${process.env.API_URL}/login`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(
+                {
+                    email: process.env.API_USER,
+                    password: process.env.API_PASS
+                }),
+        });
 
-    if (!response.ok) {
-        throw new Error(`Login failed: ${response.statusText}`);
+        if (!response.ok) {
+            throw new Error(`Falha ao logar na API: ${response.statusText}`);
+        }
+
+        let json = await response.json();
+        let token = json.user.token;
+
+        return token;
     }
 
-    let json = await response.json();
-    let token = json.user.token;
+    async novoProcesso(token: string, descricao: string): Promise<string> {
+        const response = await fetch(`${process.env.API_URL}/processes`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Authorizer ${token}`
+            },
+            body: JSON.stringify({
+                "provider": "gpt",
+                "model": "gpt-4o-mini",
+                "annotation": "Testes realizados via BFF",
+                "priority": "1",
+                "descriptions": [descricao],
+                "email": process.env.API_USER
+            }),
+        });
 
-    return token;
+        if (!response.ok) {
+            throw new Error(`Falha ao criar processo na API: ${response.statusText}`);
+        }
+
+        let json = await response.json();
+        let processId = json.id;
+
+        return processId;
+    }
 }
