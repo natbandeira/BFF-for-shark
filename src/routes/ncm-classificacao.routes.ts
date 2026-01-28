0fe89025249b14d93ef4684a5fb6319b954c10ae
aneycan <nbandeira7@gmail.com>
2026-01-28 11:07:22 -0300

    melhora tratamento de erro

--- src/routes/ncm-classificacao.routes.ts
+++ src/routes/ncm-classificacao.routes.ts
@@ -15,8 +15,11 @@ router.post('/ncm-classificacao', async (req: Request, res: Response) => {
         const resultado = await service.obterNcmPorDescricao(descricao);
         res.json({ resultado });
 
-    } catch (error) {
-        res.status(500).json({ error: 'Erro interno' });
+    } catch (error: any) {
+        return res.status(502).json({
+            error: 'Erro ao comunicar com a API externa',
+            details: error.message
+        })
     }
 });
 
