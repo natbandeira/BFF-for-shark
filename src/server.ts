731b90b66d3caff7e427fa306b62f915a120a504
aneycan <nbandeira7@gmail.com>
2026-01-28 11:00:42 -0300

    inicia server

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import ncmRoutes from './routes/ncm-classificacao.routes'
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.use(ncmRoutes);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`BFF rodando na porta ${PORT}`);
+});
