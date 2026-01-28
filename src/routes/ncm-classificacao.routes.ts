e8a6de599c63d2b93c5647e3eb71d4ca4c5fbcba
aneycan <nbandeira7@gmail.com>
2026-01-28 10:52:58 -0300

    finaliza rota

--- src/routes/ncm-classificacao.routes.ts
+++ src/routes/ncm-classificacao.routes.ts
@@ -1,1 +1,23 @@
 import { Router, Request, Response } from 'express';
+import { NcmClassificacaoService } from '../services/ncm-classificacao.service';
+
+const router = Router();
+const service = new NcmClassificacaoService();
+
+router.post('/ncm-classificacao', async (req: Request, res: Response) => {
+    try {
+        const { descricao } = req.body;
+
+        if (!descricao) {
+            return res.status(400).json({ error: 'O campo descrição é orbigatório' });
+        }
+
+        const resultado = await service.obterNcmPorDescricao(descricao);
+        res.json({ resultado });
+
+    } catch (error) {
+        res.status(500).json({ error: 'Erro interno' });
+    }
+});
+
+export default router;
