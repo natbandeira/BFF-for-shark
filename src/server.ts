be1689247524ca8dc24d2b2bd9e33f6cf2ae4c2e
aneycan <nbandeira7@gmail.com>
2026-01-28 13:58:57 -0300

    adiciona porta dinamica no server

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ app.use(express.json());
 
 app.use(ncmRoutes);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`BFF rodando na porta ${PORT}`);
 });
